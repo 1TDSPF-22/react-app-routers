f1274b2ad0282b0da4f8e5ccb9b68fbf4676e90f
phalves23 <rm93693@fiap.com.br>
2022-08-23 16:55:18 -0300

    :sparkles: Criando rotas entre as páginas

---
+++ rm93693/aula05/src/routes/MainRoutes.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "../components/home/Home";
+import ModaFeminina from "../components/modaFeminina/ModaFeminina";
+import ModaInfantil from "../components/modaInfantil/ModaInfantil";
+import ModaMasculina from "../components/modaMasculina/ModaMasculina";
+import Servicos from "../components/servicos/Servicos";
+
+export default function MainRoutes() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/moda-feminina" element={<ModaFeminina />} />
+        <Route path="/moda-masculina" element={<ModaMasculina />} />
+        <Route path="/moda-infantil" element={<ModaInfantil />} />
+        <Route path="/servicos" element={<Servicos />} />
+      </Routes>
+    </>
+  );
+}
