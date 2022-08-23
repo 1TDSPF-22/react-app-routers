752652af7161274951c5fa9d422b63d81b900974
phalves23 <rm93693@fiap.com.br>
2022-08-23 16:54:00 -0300

    Desenvolvendo componente Rodape

---
+++ rm93693/aula05/src/components/rodape/Rodape.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Rodape.css";
+
+export default function Rodape() {
+  return (
+    <div>
+      <footer className="rodape">
+        <p>Desenvolvido por &copy;PhAlves</p>
+      </footer>
+    </div>
+  );
+}
