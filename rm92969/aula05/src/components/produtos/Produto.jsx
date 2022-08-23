805861a051a9d7ce842d000adc7e9ca31ca0c835
ViníciusYoda <rm92969@fiap.com.br>
2022-08-23 14:19:22 -0300

    Rotas no react

---
+++ rm92969/aula05/src/components/produtos/Produto.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Produtos() {
+
+    const produtos = {
+        backgroundColor: '#bff',
+        height: '85vh',
+        textAlign: 'center',
+        color: '#7cc'
+    }
+
+    return (
+        <div style={produtos}>
+            <h1>Página de Produtos - PRODUTOS</h1>
+            <p>Exemplo de página de Produtos</p>
+        </div>
+    )
+}
