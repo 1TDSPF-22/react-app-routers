542358b901ff8440473c40e3d13be7cfad62d289
btwbeatriz <rm95246@fiap.com.br>
2022-08-24 15:20:33 -0300

    Estrutura de rotas básicas

---
+++ rm95246/aula05/src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+import Home from "./components/home/Home"
+import Produtos from "./components/produtos/Produto"
+import Servicos from "./components/servicos/Servicos"
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
+
+export default function App(){
+    return(
+            <BrowserRouter>
+            <ul>
+                <li><Link to='/'>Home</Link></li>
+                <li><Link to='/produtos'>Produtos</Link></li>
+                <li><Link to='/servicos'>Serviços</Link></li>
+            </ul>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/produtos" element={<Produtos/>}/>
+                    <Route path="/servicos" element={<Servicos/>}/>
+                </Routes>
+            </BrowserRouter>
+    )
+}
