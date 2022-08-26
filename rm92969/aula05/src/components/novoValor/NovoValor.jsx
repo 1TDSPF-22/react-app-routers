c4b128a504528596f75b4e81e87dd885d8198269
Vinícius Yoda <vyoda4604@gmail.com>
2022-08-26 12:12:41 -0300

    react useState

---
+++ rm92969/aula05/src/components/novoValor/NovoValor.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+import { useState} from "react"
+
+export default function NovoValor(){
+
+    const [frase, setFrase] = useState('')
+
+   // const minhaFuncao = ()=>{
+     //   setFrase("ALE")
+    // }
+    
+
+    return(
+        <div>
+            <h1>Vamos escrever uma frase</h1>
+            <div>
+                <label>Motivação</label>
+                <input type="text" name="txtFrase" onChange={()=> setFrase(e.target.value)} id="" />
+                <p>Motivação do dia : <span>{frase}</span></p>
+            </div>
+            <div>
+               
+            </div>
+        </div>
+    )
+}
--- rm92969/aula05/src/routes/MainRoutes.jsx
+++ rm92969/aula05/src/routes/MainRoutes.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Home from "../components/home/Home"
 import Produtos from "../components/produtos/Produto"
 import Servicos from "../components/servicos/Servicos"
+import NovoValor from '../components/novoValor/NovoValor'
 import { Routes, Route } from 'react-router-dom'
 export default function MainRoutes() {
     return (
@@ -10,6 +11,7 @@ export default function MainRoutes() {
                 <Route path="/" element={<Home />} />
                 <Route path="/produtos" element={<Produtos />} />
                 <Route path="/servicos" element={<Servicos />} />
+                <Route path='/nv' element={<NovoValor />} />
             </Routes>
         </>
     )
