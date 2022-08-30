0df5396d193345b804a40cdccdc81e299d575c0e
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2022-08-30 08:25:35 -0300

    Exemplo 1 de Evento

--- pf0670/aula05/src/components/novoValor/NovoValor.jsx
+++ pf0670/aula05/src/components/novoValor/NovoValor.jsx
@@ -3,23 +3,23 @@ import { useState } from "react"
 
 export default function NovoValor() {
 
-    const[frase,setFrase] = useState('')
+    const [frase, setFrase] = useState('')
 
-    // const minhaFuncao = ()=>{
+    // const minhaFuncao = (e)=>{
     //     setFrase("ALE")
     // }
 
-    return(
+    return (
         <div>
             <h1>Vamos escrever uma frase.</h1>
             <div>
                 <label>MOTIVAÇÃO: </label>
-                <input type="text" name="txtFrase" onChange={() => setFrase("Ale")}/>
+                <input type="text" name="txtFrase" onChange={(e) => setFrase(e.target.value)} />
                 <p>Motivação do dia : <span>{frase}</span></p>
             </div>
             <div>
-                
+
             </div>
         </div>
-        )
+    )
 }
